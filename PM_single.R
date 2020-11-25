828ba1f152ba9ee633ae4aa5aa7b4932a32e6023
Elliot-JG <ejg27@student.le.ac.uk>
2020-11-25 14:06:37 +0000

    Function for plotting Pm2.5 in a single location

---
+++ PM_single.R
@@ -0,0 +1,18 @@
+# A function that plots PM2.5, at a single location
+
+pm2.5_single <- function(location, years){
+  
+  Pollutant_place <- importAURN(
+    site = location, 
+    year = years, 
+    meta = TRUE)
+  
+  timePlot(Pollutant_place, 
+           pollutant = c("pm2.5"),
+           avg.time = "month",
+           y.relation = "free", 
+           ylab = "no2 (ug/m3)")
+}
+
+# Test  
+pm2.5_single("HRL", 2014:2020) 
