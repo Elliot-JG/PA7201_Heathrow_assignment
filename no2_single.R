b4cd00140b8bf3b64cae7f9b3140c156b154c05b
Elliot-JG <ejg27@student.le.ac.uk>
2020-11-25 14:07:03 +0000

    Function for plotting no2 in one location

---
+++ no2_single.R
@@ -0,0 +1,18 @@
+# A function that plots no2, at a single location
+
+no2_single <- function(location, years){
+  
+  Pollutant_place <- importAURN(
+    site = location, 
+    year = years, 
+    meta = TRUE)
+  
+  timePlot(Pollutant_place, 
+           pollutant = c("no2"),
+           avg.time = "month",
+           y.relation = "free", 
+           ylab = "no2 (ug/m3)")
+}
+
+# Test  
+no2_single("HRL", 2014:2020)  
